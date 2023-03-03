82085b8c4ff1e4b10b00bf284aadbac9be4e9d98
lucas <ljanaudis@gmail.com>
2023-03-03 14:14:11 -0300

    small update/database

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,25 @@
+/* controllers pode ter até 5 funçoes dentro deles
+    index - GET para listar vários registros.
+    show - GET para exibir um registro especifico.
+    create - POST para criar um registro.
+    update - PUT para atualizar um registro.
+    delete - DELETE para remover um registro
+*/
+
+const AppError = require("../utils/AppError");
+
+class UsersController {
+  create(request, response) {
+    const { name, email, password } = request.body;
+
+    if(!name) {
+      throw new AppError("Nome é obrigatório");
+
+    }
+
+    response.status(201).json({ name, email, password });
+  }
+
+}
+
+module.exports = UsersController;
