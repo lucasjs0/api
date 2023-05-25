a4498253f3cad386cabd2a76d80db6c0192c9548
Lucas <ljanaudis@gmail.com>
2023-05-25 01:08:59 -0300

    fix: small fix on notesController

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -5,7 +5,7 @@ class NotesController {
     const { title, description, tags, links } = request.body;
     const { user_id } = request.params;
 
-    const note_id = await knex("notes").insert({
+    const [note_id] = await knex("notes").insert({
       title,
       description,
       user_id
@@ -30,7 +30,7 @@ class NotesController {
 
     await knex("tags").insert(tagsInsert);
 
-    response.json();
+    return response.json();
   }
 
   async show(request, response) {
